03c57399e1f4cf946cb22594d4430ac25686fb54
Omajan1 <1008355@hr.nl>
2025-12-04 12:40:34 +0100

    Update fragmented.sh

--- fragmented content/fragmented.sh
+++ fragmented content/fragmented.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-NAME="--NAME--"
+NAME="fragmented"
 
 # Figure out where this script lives (scripts/)
 SCRIPT_DIR="$(pwd)"
 
 # Output folder next to scripts/
-OUTPUT_DIR="."
+OUTPUT_DIR="../output"
 
 # Paths
 IMG_PATH="${OUTPUT_DIR}/${NAME}.img"
@@ -15,8 +15,8 @@ MNT_DIR="/mnt/fs"
 
 # NORMAL OPERATION
 
-# 100 MB test image.
-dd if=/dev/zero of="$IMG_PATH" bs=1M count=--SIZE--
+# 10 MB test image.
+dd if=/dev/zero of="$IMG_PATH" bs=1M count=30
 
 # create filesystem inside image file.
 mkfs.ext4 -O ^has_journal -E lazy_itable_init=0,lazy_journal_init=0 "$IMG_PATH"
@@ -28,11 +28,13 @@ mkdir -p "$MNT_DIR"
 sudo mount -o loop "$IMG_PATH" "$MNT_DIR"
 
 # The test
-REALFILE="/path/to/original.bin" #paste here the file to copy over to test on
+REALFILE="/home/kali/Desktop/CCF proj/F-15C DCS Flaming Cliffs Flight Manual EN.pdf" #paste here the file to copy over to test on
 FRAGDIR="$MNT_DIR/fragtest"
 mkdir -p "$FRAGDIR"
 
 TARGET="$FRAGDIR/fragmented_real_file.bin"
+TARGET2="$FRAGDIR/fragmented_real_file2.bin"
+
 ALTFILE="$FRAGDIR/helper.txt"
 
 CHUNK_SIZE=4096   # 4 KB chunks tend to produce strong fragmentation
@@ -43,8 +45,9 @@ split -b $CHUNK_SIZE "$REALFILE" /tmp/fragpiece_
 echo ">> Building fragmented file..."
 for piece in /tmp/fragpiece_*; do
     cat "$piece" >> "$TARGET"
+    cat "$piece" >> "$TARGET2"
 
-    echo "x_" >> "$ALTFILE"
+    echo "x" >> "$ALTFILE"
 done
 
 sync
